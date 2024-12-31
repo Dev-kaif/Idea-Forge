a2081b99e224de3662df4b7f946a5db7fcff2fec
Mohammad Kaif <kaifghalib123@gmail.com>
2025-01-01 03:45:26 +0530

    made the database schema

---
+++ Backend/src/Database/schema.ts
@@ -0,0 +1,22 @@
+import mongoose, { Schema} from 'mongoose';
+
+
+
+const user = new Schema({
+    email : {type:String,required:true,unique:true},
+    password : {type:String,required:true},
+    firstName : String,
+    lastName : String
+})
+
+export const UserModel = mongoose.model('User',user);
+
+const Content = new Schema({
+    title:String,
+    link:String,
+    tags:[{type:mongoose.Types.ObjectId, ref:"Tag"}],
+    userId:[{type:mongoose.Types.ObjectId, ref:user}]
+})
+
+export const ContentModel = mongoose.model('Content',Content);
+
