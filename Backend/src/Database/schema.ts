b32759c7693697a5110ac597f8732ca6ead10ca1
Mohammad Kaif <kaifghalib123@gmail.com>
2025-01-01 20:39:49 +0530

    added username in schema

--- Backend/src/Database/schema.ts
+++ Backend/src/Database/schema.ts
@@ -5,6 +5,7 @@ import mongoose, { Schema} from 'mongoose';
 const user = new Schema({
     email : {type:String,required:true,unique:true},
     password : {type:String,required:true},
+    username : {type:String,required:true,unique:true},
     firstName : String,
     lastName : String
 })
