58c243ee03d9d531237aced499af65d4185e8fb8
Mohammad Kaif <kaifghalib123@gmail.com>
2025-01-02 04:26:15 +0530

    added custom colors

--- Frontend/src/Components/Background.tsx
+++ Frontend/src/Components/Background.tsx
@@ -8,8 +8,8 @@ const Background = () => {
     <div id='nav'  className=' w-full flex justify-between'>
       <div id='text' className='font-bold text-2xl md:text-4xl '>All Notes</div>
       <div className='flex gap-3'>
-      <Button text="Share Idea" variant={"bg-blue-50 hover:bg-blue-100 text-blue-600  hidden"} sidebar={false} icon={<IoShareSocialOutline />}/>
-      <Button text="Add Content" variant={"bg-blue-600 hover:bg-blue-500 "} icon={<FaPlus />}/>
+      <Button text="Share Idea" variant={"bg-purple-300 hover:bg-purple-400 text-purple-500  hidden"} sidebar={false} icon={<IoShareSocialOutline />}/>
+      <Button text="Add Content" variant={"bg-purple-700 hover:bg-purple-600 "} icon={<FaPlus />}/>
       </div>
     </div>
     <div id='cards'></div>
--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -4,7 +4,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        purple:{
+          300:"#e0e7fe",
+          400:"#c0ceff",
+          500:"#3e38a7",
+          600:"#5046f8",
+          700:"#5046e4"
+        }
+      }
+    },
   },
   plugins: [],
 }
