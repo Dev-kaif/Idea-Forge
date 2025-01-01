b81940e42e46f4336b6b5278bebb1cdbc3a4d61e
Mohammad Kaif <kaifghalib123@gmail.com>
2025-01-02 04:08:06 +0530

    made app

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,8 +1,16 @@
 
+
+import Background from './Components/Background';
+import Sidebar from './Components/sidebar';
+
+
 const App = () => {
+
+
   return (
-    <div>
-      
+    <div className='bg-zinc-800 min-h-screen text-white flex overflow-hidden transition-all duration-500'>
+      <Sidebar/>
+      <Background/>
     </div>
   )
 }
