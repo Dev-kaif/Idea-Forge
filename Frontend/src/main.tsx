c1e564a14b477583ef0f035caa40ffa4fd7b4704
Mohammad Kaif <kaifghalib123@gmail.com>
2025-01-03 03:47:15 +0530

    added react-router-dom

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './App.tsx';
+import Add from './Components/addContent.tsx'
+import Landing from './Landing Page/Landing';
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <BrowserRouter>
+    {/* <Landing/> */}
     <App />
+    {/* <Add/> */}
+    {/* <LoginPage/> */}
+    </BrowserRouter>
   </StrictMode>,
 )
