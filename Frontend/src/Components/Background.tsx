f80b454c050408e5c1e2902a28d41a18ed431a3e
Mohammad Kaif <kaifghalib123@gmail.com>
2025-01-04 04:02:11 +0530

    created the add content pop up

--- Frontend/src/Components/Background.tsx
+++ Frontend/src/Components/Background.tsx
@@ -3,14 +3,18 @@ import { IoShareSocialOutline } from "react-icons/io5";
 import { FaPlus } from "react-icons/fa6";
 import Cards from "./Cards";
 
-const Background = () => {
+interface func{
+  onClickopen:()=>void
+}
+
+const Background = ({onClickopen}:func) => {
   return (
     <div id='Background' className='w-full min-h-screen py-12 md:px-10 md:py-12'>
     <div id='nav'  className=' w-full flex justify-between px-1'>
       <div id='text' className='font-bold text-2xl md:text-4xl '>All Notes</div>
       <div className='flex gap-3'>
       <Button text="Share Idea" variant={"bg-purple-300 hover:bg-purple-400 text-purple-500  hidden"} sidebar={false} icon={<IoShareSocialOutline />}/>
-      <Button text="Add Content" variant={"bg-purple-700 hover:bg-purple-600 "} icon={<FaPlus />}/>
+      <Button onClick={()=>onClickopen()} text="Add Content" variant={"bg-purple-700 hover:bg-purple-600 "} icon={<FaPlus />}/>
       </div>
     </div>
     <div id='cards'>
--- Frontend/src/Components/Button.tsx
+++ Frontend/src/Components/Button.tsx
@@ -4,16 +4,17 @@ interface buttonProps{
     text :string ,
     variant?:string,
     icon?:ReactElement,
-    sidebar?:boolean
+    sidebar?:boolean,
+    onClick?:()=>void
 }
 
 
 
 
-const Button = ({text ,icon,variant,sidebar}:buttonProps) => {
+const Button = ({text ,icon,variant,sidebar,onClick}:buttonProps) => {
   return (
     <div>    
-        <div className={`${variant} ${sidebar && "gap-5 my-3 text-xl hover:bg-zinc-500"} md:flex flex items-center cursor-pointer rounded-lg text-sm gap-2 px-4 py-2`}>
+        <div onClick={onClick} className={`${variant} ${sidebar && "gap-5 my-3 text-xl hover:bg-zinc-500"} md:flex flex items-center cursor-pointer rounded-lg text-sm gap-2 px-4 py-2`}>
             {icon}
             <div>{text}</div>
         </div>
