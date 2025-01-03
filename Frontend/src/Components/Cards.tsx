785a454d90af2b8241511de89c01a6b592d6e563
Mohammad Kaif <kaifghalib123@gmail.com>
2025-01-03 23:32:19 +0530

    made grid for cards

--- Frontend/src/Components/Cards.tsx
+++ Frontend/src/Components/Cards.tsx
@@ -2,8 +2,11 @@ import Card from "./Card"
 
 const Cards = () => {
   return (
-    <div>
-     <Card/>
+    <div className="mx-auto px-4 py-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <Card YoutubeSrc={"https://www.youtube.com/embed/O2A4DcblBlc?si=3rtYKU26CirfLIoe"}/>
+        <Card twitterSrc={"https://x.com/kirat_tw/status/1875218603966136424" }/>
+    </div>
     </div>
   )
 }
