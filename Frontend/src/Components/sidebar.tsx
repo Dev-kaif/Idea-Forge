34f8b5767ca0ce0106e6861d620410579c14f188
Mohammad Kaif <kaifghalib123@gmail.com>
2025-01-03 20:11:56 +0530

    made open close sidebar

--- Frontend/src/Components/sidebar.tsx
+++ Frontend/src/Components/sidebar.tsx
@@ -1,13 +1,14 @@
 import { LuBrain } from "react-icons/lu";
-// import { FaPlus } from "react-icons/fa6";
 import { AiOutlineYoutube } from "react-icons/ai";
 import { FaXTwitter } from "react-icons/fa6";
 import { IoDocumentTextOutline ,IoLink} from "react-icons/io5";
 import { LiaHashtagSolid } from "react-icons/lia"
 import { MdOutlineClearAll } from "react-icons/md"
+import { FiSidebar } from "react-icons/fi";
 
 
 import Button from "./Button";
+import { useState } from "react";
 
 
 function Sidebar() {
@@ -21,17 +22,22 @@ function Sidebar() {
         {name:"Tags",logo:LiaHashtagSolid}
       ]
 
+      const [open,setOpen] = useState(true);
+
 
   return (
-    <div id='sidebar' className='bg-zinc-800 w-48 md:w-[18vw] min-h-screen py-8 '>
+    <div id='sidebar' className={`bg-zinc-800 min-h-screen py-8 transition-all duration-500 ease-in-out ${open? 'w-48 md:w-[18vw]': "w-20"} `}>
     <div id='Logo' className='w-full px-2'>
-      <div className='flex items-center text-2xl md:text-3xl gap-2 font-bold'>
+      <div className='flex items-center justify-between h-10'>
+      {open && <div className="flex items-center text-2xl md:text-3xl gap-2 font-bold">
         <LuBrain className='text-blue-300 text-3xl md:text-4xl ' />
-        <div>Idea Forge</div>
+         <div>Idea Forge</div>
+         </div>}
+        <FiSidebar onClick={()=>setOpen(prev=>!prev)} className="text-xl hover:text-gray-300"/>
       </div>
     </div>
     <div className='mt-10  w-full h-full'>
-        {data.map((items,index)=> <Button key={index} sidebar={true} text={items.name} icon={<items.logo/>} />)}
+        {data.map((items,index)=> <Button key={index} sidebar={true} variant="h-14" text={open ? items.name : ''} icon={<items.logo/>} />)}
     </div>
   </div>
   )
