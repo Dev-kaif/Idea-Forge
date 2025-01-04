815c4a6dd36643b1265575d5604ebcffa8c70bde
Mohammad Kaif <kaifghalib123@gmail.com>
2025-01-05 05:02:48 +0530

    made the card render state

--- Frontend/src/DashBoard.tsx
+++ Frontend/src/DashBoard.tsx
@@ -1,21 +1,21 @@
-
-
 import { useState } from 'react';
 import Background from './Dashboard Components/Background';
 import CreateContent from './Dashboard Components/CreateContent';
 import Sidebar from './Dashboard Components/sidebar';
 
 
-const DashBoard = () => {
-  const [modelOpen, setModelOpen] = useState(false)
+const DashBoard = ({data,shared}) => {
+  const [modelOpen, setModelOpen] = useState(false);
+  const [cardRender, setcardRender] = useState(false);
 
   return (
     <div className='bg-zinc-900 min-h-screen font-janeLight text-white flex overflow-hidden transition-all duration-500'>
       <CreateContent open={modelOpen} onClose={()=>{
         setModelOpen(false)
+        setcardRender(prev=>!prev)
       }}/>
       <Sidebar/>
-      <Background onClickopen={()=>{
+      <Background cardRender={cardRender} shared={shared} data={data} onClickopen={()=>{
         setModelOpen(true)
       }} />
     </div>
