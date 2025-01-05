f78eb8c64753c18f627a549188387a8d9ce2ac40
Mohammad Kaif <kaifghalib123@gmail.com>
2025-01-05 23:09:27 +0530

    updated the font

--- Frontend/src/Landing Page/components/home-page.tsx
+++ Frontend/src/Landing Page/components/home-page.tsx
@@ -6,7 +6,7 @@ export default function HomePage() {
     <div>
       <section className="container py-24">
           <div className="max-w-[800px] mx-auto text-center space-y-8">
-            <h1 className="text-[6vw] font-bold tracking-tight leading-[1.1]">
+            <h1 className="text-[6vw] font-bold tracking-tight">
               EMBRACE <FaArrowRightLong className="inline-block ml-2 text-5xl" />
               <span className="block">THE SMART WAY</span>
               <span className="block">OF WRITING IDEAS</span>
