09fb0895f3bc46cd4b0910b307cbacd6b6ad5099
Mohammad Kaif <kaifghalib123@gmail.com>
2025-01-06 23:56:39 +0530

    added notfound and unauthorized page

--- Frontend/src/Routes/Route.tsx
+++ Frontend/src/Routes/Route.tsx
@@ -5,6 +5,7 @@ import Landing from "../Landing Page/Landing";
 import LoginPage from "../Authorization/Login";
 import DashBoard from "../Dashboard Components/DashBoard";
 import Shared from "../Dashboard Components/Shared";
+import NotFound404 from "../Dashboard Components/Notfound404";
 
 const route = () => {
   return (
@@ -14,7 +15,8 @@ const route = () => {
         <Route path="/login" element={<LoginPage/>}/>
         <Route path="/dashboard" element={<DashBoard/>}/>
         <Route path="/dashboard/:filter" element={<DashBoard/>}/>
-        <Route path="/:hash" element={<Shared />}/>
+        <Route path="/share/:hash" element={<Shared />}/>
+        <Route path="/:random" element={<NotFound404/>}/>
     </Routes>
   )
 }
